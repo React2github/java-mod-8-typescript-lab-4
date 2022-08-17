2c37b792f00ce5f009b7bd689d173764bbd9116e
Abdella <abdella.shukur2@gmail.com>
2022-08-17 10:30:07 -0400

    lab 4 fully working

---
+++ src/index.js
@@ -0,0 +1,32 @@
+var numberInput = document.getElementById("spacesNumber");
+var stringInput = document.getElementById("stringToPrepend");
+var prependButton = document.getElementById("prependButton");
+prependButton.addEventListener("click", function () {
+    var numInputArray = numberInput.value.split(", ");
+    var stringInputArray = stringInput.value.split(", ");
+    var result = prepend(numInputArray, stringInputArray);
+    console.log(result);
+});
+function prepend(numInputArray, stringInputArray) {
+    var numSpaces = numInputArray.map(function (str) {
+        return Number(str);
+    });
+    var wordsArray = stringInputArray.map(function (str) {
+        return String(str);
+    });
+    var result = "";
+    for (var i = 0; i < wordsArray.length; i++) {
+        var inputString = wordsArray[i];
+        var spacesNumber = numSpaces[i];
+        console.log(inputString);
+        console.log(spacesNumber);
+        var spacesString = "";
+        for (var i_1 = 0; i_1 < spacesNumber; i_1++) {
+            spacesString += "&nbsp";
+        }
+        result += spacesString + inputString;
+        console.log(result);
+    }
+    var resultString = result;
+    return resultString;
+}
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,43 @@
-// your code here
+const numberInput = document.getElementById("spacesNumber") as HTMLInputElement;
+const stringInput = document.getElementById("stringToPrepend") as HTMLInputElement;
+const prependButton = document.getElementById("prependButton") as HTMLButtonElement;
+
+
+
+prependButton!.addEventListener("click", function () {
+const numInputArray = numberInput.value.split(", ");
+const stringInputArray = stringInput.value.split(", ");
+
+    let result = prepend(numInputArray, stringInputArray);
+    console.log(result)
+
+});
+
+
+function prepend<T>(
+numInputArray: T[],
+stringInputArray: T[]
+) {
+let numSpaces = numInputArray.map(str => {
+return Number(str);
+})
+let wordsArray = stringInputArray.map(str => {
+return String(str);
+})
+    let result = "";
+    for (let i=0; i < wordsArray.length; i++) {
+        let inputString = wordsArray[i];
+         let spacesNumber = numSpaces[i];
+console.log(inputString)
+console.log(spacesNumber)
+    let spacesString: String = ""
+    for (let i=0; i < spacesNumber; i++) {
+        spacesString += "&nbsp";
+    }
+    result += spacesString + inputString;
+    console.log(result)
+    }
+    let resultString: String = result;
+
+    return resultString
+}
